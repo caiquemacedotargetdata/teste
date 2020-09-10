68843d554039109191282ad269586f9c668ca5f1
caiquemacedotargetdata <caique.macedo@targetdata.com.br>
2020-09-09 21:51:30 -0300

    Teste

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,7 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 if [ -z "$NEEDS_TAG" ]; then
     git tag $NEW_TAG
     echo "Tagged with $NEW_TAG"
-    git push https://${USERNAME}:${PASSWORD}@github.com/targetdata/teste.git --tags
+    git push https://${USERNAME}:${PASSWORD}@github.com/caiquemacedotargetdata/teste.git --tags
 else
     echo "Already a tag on this commit"
 fi
