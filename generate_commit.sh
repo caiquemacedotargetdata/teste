f846a92a5f7dd0eaf84cd31a8bda650fa4cd5397
caiquemacedotargetdata <caique.macedo@targetdata.com.br>
2020-09-09 23:43:04 -0300

    Teste official

--- generate_commit.sh
+++ generate_commit.sh
@@ -14,7 +14,7 @@ if [ "$1" ==  "-u" ] && [ "$3" ==  "-p" ] && [ "$5" ==  "-m" ] && [ ! -z "$MESSA
 	git push https://${USERNAME}:${PASSWORD}@github.com/${URI_PROJECT} --all
 	
 	# Call other bash script to generate a tag in git
-	bash ./generate_tag.sh $1 USERNAME $3 PASSWORD $VERSION_OR_RELEASE
+	bash ./generate_tag.sh $1 $USERNAME $3 $PASSWORD $VERSION_OR_RELEASE
 
 else
 	echo "Sorry, Need put the message to commit"
