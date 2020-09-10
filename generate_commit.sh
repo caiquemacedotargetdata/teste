ab536c5174bceb05e70e779a0088e1cec5e12e58
caiquemacedotargetdata <caique.macedo@targetdata.com.br>
2020-09-10 00:12:04 -0300

    Teste official

--- generate_commit.sh
+++ generate_commit.sh
@@ -8,14 +8,15 @@ PASSWORD=$4
 MESSAGE=$6
 VERSION_OR_RELEASE=$7
 
-if [ "$1" ==  "-u" ] && [ "$3" ==  "-p" ] && [ "$5" ==  "-m" ] && [ ! -z "$MESSAGE" ] && [ ! -z "$VERSION_OR_RELEASE" ]; then
+if [ "$1" ==  "-u" ] && [ "$3" ==  "-p" ] && [ "$5" ==  "-m" ] && [ ! -z "$MESSAGE" ]; then
 	git add .
 	git commit -m "$MESSAGE"
 	git push https://${USERNAME}:${PASSWORD}@github.com/${URI_PROJECT} --all
 	
-	# Call other bash script to generate a tag in git
-	bash ./generate_tag.sh $1 $USERNAME $3 $PASSWORD $VERSION_OR_RELEASE
-
+	if [ ! -z "$VERSION_OR_RELEASE" ]; then
+		# Call other bash script to generate a tag in git
+		bash ./generate_tag.sh $1 $USERNAME $3 $PASSWORD $VERSION_OR_RELEASE
+	fi
 else
 	echo "Sorry, Need put the message to commit"
 fi
