1763788b139b985f0eb3a7ae9c3b4eea5d88317a
caiquemacedotargetdata <caique.macedo@targetdata.com.br>
2020-09-09 23:32:33 -0300

    Teste official

--- generate_commit.sh
+++ generate_commit.sh
@@ -7,14 +7,14 @@ URI_PROJECT="caiquemacedotargetdata/teste.git"
 
 MESSAGE=$2
 VERSION_OR_RELEASE=$3
-echo $MESSAGE
+
 if [ "$1" ==  "-m" ] && [ ! -z "$MESSAGE" ] && [ ! -z "$VERSION_OR_RELEASE" ]; then
 	git add .
 	git commit -m "$MESSAGE"
 	git push https://${USERNAME}:${PASSWORD}@github.com/${URI_PROJECT} --all
 	
 	# Call other bash script to generate a tag in git
-	sh ./generate_tag.sh VERSION_OR_RELEASE
+	bash ./generate_tag.sh $VERSION_OR_RELEASE
 
 else
 	echo "Sorry, Need put the message to commit"
